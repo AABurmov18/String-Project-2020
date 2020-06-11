0dfb0acefa1826a157ab783e3012ca794e243352
Magdalena <miomayska18@codinburgas.bg>
2020-06-11 19:16:44 +0300

    and stefan talked, added some missing code and moved a function

--- String Project 2020.cpp
+++ String Project 2020.cpp
@@ -13,6 +13,8 @@ struct ADMINISTRATOR
     string stUsername;
     string stPass;
 
+	string reg[100];
+
     string trUsername;
     string trPass;
 };
@@ -25,8 +27,11 @@ struct QUIZ
 
     double time;
 
+	string quiz[100];
+
     string stAnswers;
 };
+
 
 // ---------------------------------------------------------------------------- Administrator ----------------------------------------------------------------------------
 
@@ -87,7 +92,7 @@ void viewStudentaccounts(ADMINISTRATOR* reg, int stCounter, int& maxId)
     }
 }
 
-bool listDeleteAccountsMenu(ADMINISTRATOR* reg, int trCounter, int& maxId, int stCounter, int searchID)
+void listDeleteAccountsMenu(ADMINISTRATOR* reg, int trCounter, int& maxId, int stCounter, int searchID)
 {
     cout << endl;
     cout << "|==========================|" << endl;
@@ -186,6 +191,7 @@ retry4:
         cout << "Incorrect input" << endl;
         goto retry4;
     }
+	return true;
 }
 
 bool checkSystemAdministrator(string adminPass, string adminpasstry, ADMINISTRATOR* reg, int stCounter, int trCounter, int& maxId, int searchID)
@@ -206,10 +212,10 @@ retry4:
         cout << "Try Again !" << endl;
         goto retry4;
     }
+	return true;
 }
 
 // ---------------------------------------------------------------------------- Student ----------------------------------------------------------------------------
-
 
 
 void viewQuizName(QUIZ* quiz, int quizcount, int& maxId)
@@ -222,6 +228,21 @@ void viewQuizName(QUIZ* quiz, int quizcount, int& maxId)
         cout << "Quiz ID: "; cout << maxId;
         cout << "|================================================|" << endl;
     }
+}
+
+int findIdQuiz(QUIZ* quiz, int quizcount, int& maxId, int searchID)
+{
+	cout << endl;
+	cout << "|===============================|" << endl;
+	cout << "Enter the quiz ID: "; cin >> searchID;
+	cout << "|===============================|" << endl;
+	for (int i = 0; i < quizcount; i++)
+	{
+		if (searchID == maxId)
+		{
+			return i;
+		}
+	}
 }
 
 void startQuiz(QUIZ* quiz, int quizcount, int& maxId, int searchID, int stanswercount)
@@ -326,6 +347,7 @@ retry3:
         cout << "Incorrect input" << endl;
         goto retry3;
     }
+	return true;
 }
 
 bool checkSystemStudent(string stUsernameTry, string stpasstry, QUIZ* quiz, int quizcount, int& maxId, int searchID, int stanswercount, int AnswerCounterStudent, ADMINISTRATOR* reg, int stCounter)
@@ -349,25 +371,13 @@ retry10:
             goto retry10;
         }
     }
+	return true;
 }
 
 
 // ---------------------------------------------------------------------------- Teacher ----------------------------------------------------------------------------
 
-int findIdQuiz(QUIZ* quiz, int quizcount, int& maxId, int searchID)
-{
-    cout << endl;
-    cout << "|===============================|" << endl;
-    cout << "Enter the quiz ID: "; cin >> searchID;
-    cout << "|===============================|" << endl;
-    for (int i = 0; i < quizcount; i++)
-    {
-        if (searchID == maxId)
-        {
-            return i;
-        }
-    }
-}
+
 
 void deleteQuiz(QUIZ* quiz, int quizcount, int& maxId, int searchID)
 {
@@ -393,7 +403,7 @@ void viewQuiz(QUIZ* quiz, int quizcount, int& maxId)
     }
 }
 
-void createQuiz(QUIZ* quiz, int quizcount, int& maxId)
+bool createQuiz(QUIZ* quiz, int quizcount, int& maxId)
 {
     int amount;
 
@@ -422,6 +432,7 @@ retry7:
         cout << "Incorrect Input" << endl;
         goto retry7;
     }
+	return true;
 }
 
 bool teacherMenu(QUIZ* quiz, int quizcount, int& maxId, int searchID)
@@ -467,6 +478,7 @@ retry3:
         cout << "Incorrect input" << endl;
         goto retry3;
     }
+	return true;
 }
 
 bool checkSystemTeacher(string trPasstry, QUIZ* quiz, int quizcount, int& maxId, int searchID, string trUsernameTry, ADMINISTRATOR* reg, int trCounter)
@@ -491,6 +503,7 @@ retry2:
             goto retry2;
         }
     }
+	return true;
 }
 
 bool mainMenu(string trPasstry, string stUsernameTry, string stpasstry, string adminPass, string adminpasstry, ADMINISTRATOR* reg, int stCounter, int trCounter, int searchID, int& maxId, int quizcount, QUIZ* quiz, int stanswercount, int AnswerCounterStudent, string trUsernameTry)
@@ -526,10 +539,14 @@ retry1:
         cout << "Incorrect input" << endl;
         goto retry1;
     }
+	return true;
 }
 
 int main()
 {
+	ADMINISTRATOR reg[100];
+	QUIZ quiz[100];
+
     int quizcount = 0;
     int stanswercount = 0;
     int AnswerCounterStudent = 0;
@@ -549,4 +566,10 @@ int main()
 
     int maxId = 0;
     int searchID = 0;
+
+	bool showMainMenu = true;
+
+	do {
+		showMainMenu = mainMenu(trPasstry, stUsernameTry, stpasstry, adminPass, adminpasstry, reg, stCounter, trCounter, searchID, maxId, quizcount, quiz, stanswercount, AnswerCounterStudent, trUsernameTry);
+	} while (showMainMenu);
 }
